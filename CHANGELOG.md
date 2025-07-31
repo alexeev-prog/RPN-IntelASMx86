0f927784fb6a97bacdd1331a772994bbe3bdad8e
Alexeev Bronislav <alexeev.dev@mail.ru>
2025-07-31 22:44:24 +0700

    add shell.nix

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+commit da224b7e8d19bc6361b840e9a3d66062f1646da4
+Author: Alexeev Bronislav <alexeev.dev@mail.ru>
+Date:   Mon Jun 16 02:35:38 2025 +0700
+
+    move from old deprecated account
+
+commit bf3dc2bcd2a4f2f95a3c8344b98a0d5c0a884bb7
+Author: Alexeev Bronislav <alexeev.dev@mail.ru>
+Date:   Mon Jun 16 02:34:05 2025 +0700
+
+    Initial commit
---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+	name = "c-dev-env";
+
+	nativeBuildInputs = with pkgs; [
+		gcc
+		gnumake
+		nasm
+		binutils
+		pkg-config
+		valgrind
+		gdb
+	];
+
+	buildInputs = with pkgs; [
+		glibc
+		zlib
+	];
+
+	shellHook = ''
+		export CC=gcc
+		export C_INCLUDE_PATH=${pkgs.glibc.dev}/inlcude:$C_INCLUDE_PATH
+	'';
+}
+
